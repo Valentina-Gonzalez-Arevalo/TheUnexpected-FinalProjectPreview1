2d136efd83d443226381c64b8874ae86d65a5d0a
D. Valentina Gonzalez Arevalo <valeg981000@gmail.com>
2018-10-28 19:38:10 -0500

    Primer avance del proyecto final de video juego

---
+++ Assets/Scripts/Scene01Buttons.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene01Buttons : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		public void OnTap JamonButton
+		public void OnTap SopaButton
+
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/TimerScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TimerScript : MonoBehaviour {
+
+	public Text contador;
+	private float tiempo = 10f;
+
+	// Use this for initialization
+	void Start () {
+		contador.text = "" + tiempo;
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		tiempo -= Time.deltaTime;
+		contador.text = "" + tiempo.ToString("f0");
+		
+	}
+}
